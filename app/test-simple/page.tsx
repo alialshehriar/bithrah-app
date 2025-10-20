4df0210fc5b6f539f03c4dd86ea5c0fd76daa0cb
Ali Alshehri <ali.alshehri.ar@gmail.com>
2025-10-19 21:04:28 -0400

    test: Add minimal test page without CSS

---
+++ app/test-simple/page.tsx
@@ -0,0 +1,11 @@
+export default function TestSimplePage() {
+  return (
+    <html>
+      <body>
+        <h1>Test Page - No CSS</h1>
+        <p>If you see this, Next.js is working!</p>
+      </body>
+    </html>
+  );
+}
+
